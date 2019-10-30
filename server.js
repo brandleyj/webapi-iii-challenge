1ae19f7f1f3748efa29e24d68b2228863e6b4f94
Justin Brandley <brandleyj@gmail.com>
2019-10-30 14:56:30 -0500

    wrote custom middleware

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
+server.use(express.json());
+
+server.use("/users", userRouter);
+server.use(logger);
 
 server.get("/", (req, res) => {
 	res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,13 @@ server.get("/", (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	console.log(
+		`[${new Date().toISOString()}] ${req.method} to ${
+			req.originalUrl
+		} from ${req.get("Origin")}`
+	);
+	next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,58 @@
-const express = 'express';
+const express = "express";
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.post("/:id/posts", (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {});
 
-});
+router.get("/:id", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-
-};
+	const { id } = req.params;
+	userDb
+		.getById(Number(id))
+		.then(res => {
+			if (res) {
+				req.user = res;
+				next();
+			} else {
+				res.status(500).json({ message: "invalid user id" });
+			}
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ message: "The user information could not be modified." });
+		});
+}
 
 function validateUser(req, res, next) {
-
-};
+	if (!req.body) {
+		res.status(400).json({ message: "missing body data" });
+	}
+	if (!req.body.name) {
+		res.status(400).json({ messsage: "missing name data" });
+	}
+}
 
 function validatePost(req, res, next) {
-
-};
+	if (!req.body.text) {
+		res.status(400).json({ message: "missing name data" });
+	}
+	if (!req.body) {
+		res.status(400).json({ message: "missing body data" });
+	}
+}
 
 module.exports = router;
